1d2b33d47db38d84a853bd66e614536bae4e0c2a
Tobias Olea <tobiolea97@gmail.com>
2024-01-20 15:40:31 -0300

    Initialization of Data microservice

---
+++ data/src/test/java/com/daily/practice/data/DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.practice.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
