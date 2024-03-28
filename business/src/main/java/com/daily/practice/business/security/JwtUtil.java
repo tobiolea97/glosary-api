e6711bc5bb552ee81f081495b6584f5fc455595d
Tobias Olea <tobiolea97@gmail.com>
2024-03-27 23:47:39 -0300

    [BS] Security working on the microservice!

--- business/src/main/java/com/daily/practice/business/security/JwtUtil.java
+++ business/src/main/java/com/daily/practice/business/security/JwtUtil.java
@@ -1,47 +1,19 @@
 package com.daily.practice.business.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.JWT_EXPIRATION_TOKEN))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_FIRMA)
-                .compact();
-    }
-
-    public String generateToken(Authentication authentication) {
-        String username = authentication.getName();
-        Date now = new Date();
-        Date dueDate = new Date(now.getTime() + SecurityConstants.JWT_EXPIRATION_TOKEN);
-        String token = Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(now)
-                .setExpiration(dueDate)
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_FIRMA)
-                .compact();
-        return token;
-    }
-
-    public String getUsernameFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.JWT_FIRMA)
-                .parseClaimsJws(token)
-                .getBody();
-        return claims.getSubject();
-    }
-
-    public Boolean validateToken(String token) {
+    public Boolean isValid(String token) {
         try {
             Jwts.parser().setSigningKey(SecurityConstants.JWT_FIRMA).parseClaimsJws(token);
             return true;
@@ -49,4 +21,13 @@ public class JwtUtil {
             throw new AuthenticationCredentialsNotFoundException("Invalid token");
         }
     }
+
+    public String getUsernameByToken (String jwt) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWT_FIRMA)
+                .parseClaimsJws(jwt)
+                .getBody();
+        return claims.getSubject();
+    }
+
 }
