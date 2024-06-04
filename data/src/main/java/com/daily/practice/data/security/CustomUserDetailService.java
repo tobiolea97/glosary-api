f8dd6b1f262c883ed515502cb6d2ae4809aafd41
Tobias Olea <tobiolea97@gmail.com>
2024-06-04 08:05:39 -0300

    [DS] Adding CustomUserService class

---
+++ data/src/main/java/com/daily/practice/data/security/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.daily.practice.data.security;
+
+import com.daily.practice.data.domain.security.User;
+import com.daily.practice.data.repository.contract.IAuthenticationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final IAuthenticationRepository authenticationRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  authenticationRepository.getAuthentication(username);
+        if(user == null)
+            throw new UsernameNotFoundException("Registro no encontrado");
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
